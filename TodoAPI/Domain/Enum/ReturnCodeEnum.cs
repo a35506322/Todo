1125f2db37777b8a4b0d2696fe983b7da7501a14
chunglijung <andy28572463@gmail.com>
2024-04-04 15:32:45 +0800

    fix: 修正enum 代碼

--- TodoAPI/Domain/Enum/ReturnCodeEnum.cs
+++ TodoAPI/Domain/Enum/ReturnCodeEnum.cs
@@ -5,7 +5,7 @@ public enum ReturnCodeEnum
     Success = 2000,
     VaildDataError = 4000,
     QueryNotFound = 4001,
-    UnAuth = 4001,
+    UnAuth = 4002,
     ServerError = 5000,
     DBCommandFail = 5001,
 }
