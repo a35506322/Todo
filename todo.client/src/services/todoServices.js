61b33e621de2b336d2da001d47d37afeb145ef1d
chunglijung <andy28572463@gmail.com>
2024-04-06 02:39:12 +0800

    feat: 串接API complete

--- todo.client/src/services/todoServices.js
+++ todo.client/src/services/todoServices.js
@@ -1,3 +1,11 @@
 import { todoInstance } from '../utils/todoInstance.js';
 
-export const getTodo = (quertString = '') => todoInstance.get(`/api/todo/get?${quertString}`);
+export const todoService = {
+  getTodo: (quertString = '') => todoInstance.get(`/api/todo/get?${quertString}`),
+  insertTodo: (data) => todoInstance.post('/api/todo/insertTodo', data),
+  updateTodoStatus: (todoId, data) =>
+    todoInstance.put(`/api/todo/updateTodoStatus/${todoId}`, data),
+  deleteTodo: (todoId) => todoInstance.delete(`/api/todo/delete/${todoId}`),
+  updateTodoContent: (todoId, data) =>
+    todoInstance.put(`/api/todo/updateTodoContent/${todoId}`, data)
+};
--- todo.client/src/views/HomeView.vue
+++ todo.client/src/views/HomeView.vue
@@ -1,17 +1,123 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { initFlowbite } from 'flowbite';
-import { getTodo } from '@/services/todoServices';
+import { todoService } from '@/services/todoServices';
+import moment from 'moment';
+import { returnCodeEnum } from '@/utils/enums';
 
-const msg = ref('hello world');
-
-const getTodoService = async () => {
-  const result = await getTodo();
-  console.log(result);
+const todos = ref([]);
+const getTodo = async () => {
+  const result = await todoService.getTodo();
+  const { returnData } = result.data;
+  todos.value = [...returnData];
 };
-getTodoService();
+
+const request = ref({
+  name: '',
+  title: '',
+  todoContent: ''
+});
+
+const initRequest = () => {
+  isInsertStatus.value = {
+    isInsert: true,
+    todoId: ''
+  };
+  request.value = {
+    name: '',
+    title: '',
+    todoContent: ''
+  };
+};
+
+const isInsertStatus = ref({
+  isInsert: true,
+  todoId: ''
+});
+
+const currentRequest = (value) => {
+  isInsertStatus.value = {
+    isInsert: false,
+    todoId: value.todoId
+  };
+  request.value = {
+    name: value.name,
+    title: value.title,
+    todoContent: value.todoContent
+  };
+};
+
+const onComplete = async (todo) => {
+  try {
+    const result = await todoService.updateTodoStatus(todo.todoId, {
+      todoId: todo.todoId,
+      isComplete: 'Y'
+    });
+    const { returnCode, returnMessage } = result.data;
+
+    switch (returnCode) {
+      case returnCodeEnum.success:
+        await getTodo();
+        break;
+      default:
+        alert(returnMessage);
+        break;
+    }
+  } catch (error) {
+    console.error('onSubmit', error);
+    alert('意外狀況');
+  }
+};
+
+const onDelete = async (todo) => {
+  try {
+    const result = await todoService.deleteTodo(todo.todoId);
+    const { returnCode, returnMessage } = result.data;
+
+    switch (returnCode) {
+      case returnCodeEnum.success:
+        await getTodo();
+        break;
+      default:
+        alert(returnMessage);
+        break;
+    }
+  } catch (error) {
+    console.error('onSubmit', error);
+    alert('意外狀況');
+  }
+};
+
+const onSubmit = async () => {
+  try {
+    let result;
+    if (isInsertStatus.value.isInsert) {
+      result = await todoService.insertTodo(request.value);
+    } else {
+      const todoId = isInsertStatus.value.todoId;
+      result = await todoService.updateTodoContent(todoId, { todoId: todoId, ...request.value });
+    }
+
+    const { returnCode, returnMessage } = result.data;
+
+    switch (returnCode) {
+      case returnCodeEnum.success:
+        await getTodo();
+        initRequest();
+        break;
+      default:
+        alert(returnMessage);
+        break;
+    }
+  } catch (error) {
+    console.error('onSubmit', error);
+    alert('意外狀況');
+  }
+};
+
 // initialize components based on data attribute selectors
-onMounted(() => {
+onMounted(async () => {
+  await getTodo();
   initFlowbite();
 });
 </script>
@@ -21,39 +127,62 @@ onMounted(() => {
     <div class="container mx-auto px-4">
       <div class="grid grid-cols-5 gap-4">
         <div class="col-span-2">
-          <div
+          <form
             class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8"
+            @submit.prevent="onSubmit"
           >
             <div class="space-y-6">
               <h5 class="text-xl font-medium text-gray-900">請填寫代辦事項</h5>
               <div>
                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900">姓名</label>
-                <input type="text" name="name" id="name" class="input-success" required />
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  class="input-success"
+                  required
+                  v-model="request.name"
+                />
               </div>
               <div>
                 <label for="title" class="block mb-2 text-sm font-medium text-gray-900"
                   >代辦標題</label
                 >
-                <input type="text" name="title" id="title" class="input-success" required />
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  class="input-success"
+                  required
+                  v-model="request.title"
+                />
               </div>
               <div>
-                <label for="title-content" class="block mb-2 text-sm font-medium text-gray-900"
+                <label for="todo-content" class="block mb-2 text-sm font-medium text-gray-900"
                   >代辦內容</label
                 >
                 <textarea
-                  id="title-content"
-                  name="title-content"
+                  id="todo-content"
+                  name="todo-content"
                   rows="3"
                   class="textarea-success"
                   required
+                  v-model="request.todoContent"
                 ></textarea>
               </div>
               <button type="submit" class="btn-success btn-medium w-full">儲存</button>
             </div>
-          </div>
+          </form>
         </div>
         <div class="col-span-3">
-          <div id="alert-additional-content-3" class="alert-success" role="alert">
+          <div
+            :id="`alert-additional-content-${index}`"
+            :class="[todo.isComplete === 'Y' ? 'alert-success' : 'alert-second']"
+            role="alert"
+            v-for="(todo, index) in todos"
+            :key="todo.todoId"
+            class="mb-3"
+          >
             <div class="flex items-center">
               <svg
                 class="w-[24px] h-[24px] me-2"
@@ -65,60 +194,57 @@ onMounted(() => {
                 viewBox="0 0 24 24"
               >
                 <path
+                  v-if="todo.isComplete === 'Y'"
                   fill-rule="evenodd"
                   d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z"
                   clip-rule="evenodd"
                 />
-              </svg>
-
-              <span class="sr-only">Complete</span>
-              <h3 class="text-lg font-medium">莊理竣 / 1900吃飯</h3>
-              <h3 class="ml-auto text-lg font-medium">已完成 | 2024/04/05 17:00</h3>
-            </div>
-            <div class="mt-2 mb-4 text-sm">與朋友去吃好吃的</div>
-            <div class="flex justify-end">
-              <button
-                type="button"
-                class="btn-outline-success btn-small"
-                data-dismiss-target="#alert-additional-content-3"
-                aria-label="Close"
-              >
-                刪除
-              </button>
-            </div>
-          </div>
-          <div id="alert-additional-content-5" class="alert-second" role="alert">
-            <div class="flex items-center">
-              <svg
-                class="w-[24px] h-[24px] text-gray-800 dark:text-white me-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
                 <path
+                  v-else
                   fill-rule="evenodd"
                   d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z"
                   clip-rule="evenodd"
                 />
               </svg>
 
-              <span class="sr-only">Not Complete</span>
-              <h3 class="text-lg font-medium">莊理竣 / 1900吃飯</h3>
-              <h3 class="ml-auto text-lg font-medium">未完成</h3>
+              <span class="sr-only">
+                {{ todo.isComplete === 'Y' ? 'Complete' : 'Not Complete' }}
+              </span>
+              <h3 class="text-lg font-medium">{{ todo.name }}/ {{ todo.title }}</h3>
+              <h3 class="ml-auto text-lg font-medium">
+                {{ todo.isComplete === 'Y' ? '已完成' : '未完成' }}
+                {{
+                  todo.completeTime
+                    ? `| ${moment(todo.completeTime).format('YYYY/MM/DD HH:mm:ss')}`
+                    : ''
+                }}
+              </h3>
             </div>
-            <div class="mt-2 mb-4 text-sm">星期日要去吃飯</div>
+            <div class="mt-2 mb-4 text-sm">{{ todo.todoContent }}</div>
             <div class="flex justify-end">
-              <button type="button" class="btn-second btn-small inline-flex items-center mr-2">
+              <button
+                type="button"
+                class="btn-second btn-small inline-flex items-center mr-2"
+                v-if="todo.isComplete === 'N'"
+                @click="onComplete(todo)"
+              >
                 完成
               </button>
               <button
                 type="button"
-                class="btn-outline-second btn-small"
-                data-dismiss-target="#alert-additional-content-5"
+                class="btn-small inline-flex items-center mr-2"
+                :class="[todo.isComplete === 'Y' ? 'btn-success' : 'btn-second']"
+                @click="currentRequest(todo)"
+              >
+                編輯
+              </button>
+              <button
+                type="button"
+                class="btn-small"
+                :class="[todo.isComplete === 'Y' ? 'btn-outline-success' : 'btn-outline-second']"
+                :data-dismiss-target="`#alert-additional-content-${index}`"
                 aria-label="Close"
+                @click="onDelete(todo)"
               >
                 刪除
               </button>
