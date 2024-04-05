542ae9ff4e5081f64348621686280b65a1079a0e
chunglijung <andy28572463@gmail.com>
2024-04-05 10:27:35 +0800

    feat: UI 切板完成

--- todo.client/src/views/HomeView.vue
+++ todo.client/src/views/HomeView.vue
@@ -11,90 +11,117 @@ onMounted(() => {
 </script>
 
 <template>
-  <h1 className="text-3xl font-bold underline">
-    {{ msg }}
-  </h1>
-
-  <div>
-    <div class="flex justify-center p-4">
-      <button
-        id="button"
-        data-modal-toggle="modal"
-        data-modal-target="modal"
-        type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-      >
-        Show modal
-      </button>
-    </div>
-
-    <div
-      id="modal"
-      tabindex="-1"
-      aria-hidden="true"
-      class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
-    >
-      <div class="relative w-full max-w-2xl max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <!-- Modal header -->
-          <div class="flex items-start justify-between p-5 border-b rounded-t dark:border-gray-600">
-            <h3 class="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white">
-              Terms of Service
-            </h3>
-            <button
-              id="closeButton"
-              data-modal-hide="modal"
-              type="button"
-              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            >
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="container mx-auto px-4">
+      <div class="grid grid-cols-5 gap-4">
+        <div class="col-span-2">
+          <div
+            class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8"
+          >
+            <div class="space-y-6">
+              <h5 class="text-xl font-medium text-gray-900">請填寫代辦事項</h5>
+              <div>
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900">姓名</label>
+                <input type="text" name="name" id="name" class="input-success" required />
+              </div>
+              <div>
+                <label for="title" class="block mb-2 text-sm font-medium text-gray-900"
+                  >代辦標題</label
+                >
+                <input type="text" name="name" id="name" class="input-success" required />
+              </div>
+              <div>
+                <label for="title-content" class="block mb-2 text-sm font-medium text-gray-900"
+                  >代辦內容</label
+                >
+                <textarea
+                  id="title-content"
+                  name="title-content"
+                  rows="3"
+                  class="textarea-success"
+                  required
+                ></textarea>
+              </div>
+              <button type="submit" class="btn-success btn-medium w-full">儲存</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-span-3">
+          <div id="alert-additional-content-3" class="alert-success" role="alert">
+            <div class="flex items-center">
               <svg
-                class="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
+                class="w-[16px] h-[16px] me-2"
+                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
               >
                 <path
                   fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z"
                   clip-rule="evenodd"
-                ></path>
+                />
               </svg>
-            </button>
+
+              <span class="sr-only">Complete</span>
+              <h3 class="text-lg font-medium">莊理竣 / 1900吃飯</h3>
+              <h3 class="ml-auto text-lg font-medium">已完成 | 2024/04/05 17:00</h3>
+            </div>
+            <div class="mt-2 mb-4 text-sm">與朋友去吃好吃的</div>
+            <div class="flex justify-end">
+              <button
+                type="button"
+                class="btn-outline-success btn-small"
+                data-dismiss-target="#alert-additional-content-3"
+                aria-label="Close"
+              >
+                刪除
+              </button>
+            </div>
           </div>
-          <!-- Modal body -->
-          <div class="p-6 space-y-6">
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new consumer privacy
-              laws for its citizens, companies around the world are updating their terms of service
-              agreements to comply.
-            </p>
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on
-              May 25 and is meant to ensure a common set of data rights in the European Union. It
-              requires organizations to notify users as soon as possible of high-risk data breaches
-              that could personally affect them.
-            </p>
-          </div>
-          <!-- Modal footer -->
-          <div
-            class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"
-          >
-            <button
-              type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              I accept
-            </button>
-            <button
-              type="button"
-              class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600"
-            >
-              Decline
-            </button>
+          <div id="alert-additional-content-5" class="alert-second" role="alert">
+            <div class="flex items-center">
+              <svg
+                class="w-[16px] h-[16px] text-gray-800 dark:text-white me-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+
+              <span class="sr-only">Not Complete</span>
+              <h3 class="text-lg font-medium">莊理竣 / 1900吃飯</h3>
+              <h3 class="ml-auto text-lg font-medium">未完成</h3>
+            </div>
+            <div class="mt-2 mb-4 text-sm">星期日要去吃飯</div>
+            <div class="flex justify-end">
+              <button type="button" class="btn-second btn-small inline-flex items-center mr-2">
+                完成
+              </button>
+              <button
+                type="button"
+                class="btn-outline-second btn-small"
+                data-dismiss-target="#alert-additional-content-5"
+                aria-label="Close"
+              >
+                刪除
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped></style>
